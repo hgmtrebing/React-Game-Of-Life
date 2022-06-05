33b9403106a0e7623ce3643047970c959da76c26
Harry Geoffrey Trebing <hgmtrebing@outlook.com>
2022-06-05 18:28:24 -0400

    Addition of React/Typescript Frontend, as well as Webpack Infrastructure.

---
+++ src/app.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+type AppProps = {
+
+}
+
+type AppState = {
+
+}
+
+class App extends React.Component<AppProps, AppState>{
+
+    render() {
+        return (
+            <p>test wut wut</p>
+        );
+    }
+}
+
+export default App;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 app.use("/", express.static("public"));
 app.use("resources", express.static("resources"));
 
+var distPath = path.join(__dirname, "../dist")
+app.use("/dist", express.static(distPath));
+
 app.listen(3000, () => {
-    console.log("Game of Life Server listening on Port #3000.");
+    console.log("Game of Life Server listening on Port #3000!");
 });
 
